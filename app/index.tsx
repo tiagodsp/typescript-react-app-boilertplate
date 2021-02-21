b509bc6fdd2d7983cdafd986209465330c09020a
Tiago Patrocinio <tiagodsp93@gmail.com>
2021-02-22 00:01:03 +0100

    adding Webpack rules to resolve modules paths. Improvements os Eslint config.

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-import { render } from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-
-import App from "./containers/App"; // Main application
-import allReducers from "./reducers"; // All reducers combined
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from 'containers/App'; // Main application
+import allReducers from './reducers'; // All reducers combined
 
 // Create application store
 /* eslint-disable no-underscore-dangle */
-export const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 /* eslint-enable */
 
 // Render applicaiton
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,56 @@
-const path = require('path');
 // Load the plugin in your webpack.config.js file.
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
-let modules = ["app"];
-let multipleHtmlPlugins = modules.map((name) => {
-    return new HtmlWebpackPlugin({
-        template: `./${name}/public/index.html`, // relative path to the HTML files
-        filename: `${name}/index.html`, // output HTML files
-        chunks: [`${name}`], // respective JS files
-    });
-});
+const modules = ['app'];
+const multipleHtmlPlugins = modules.map((name) => new HtmlWebpackPlugin({
+  template: `./${name}/public/index.html`, // relative path to the HTML files
+  filename: `${name}/index.html`, // output HTML files
+  chunks: [`${name}`], // respective JS files
+}));
 
 module.exports = {
-    mode: "development",
-    entry: {
-        app: "./app/",
-        // app2: "./app2/",
-    },
-    output: {
-        filename: "[name]/[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            // Add HTML loader to serve HTML files.
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                    },
-                ],
-            },
+  mode: 'development',
+  entry: {
+    app: './app/',
+  },
+  output: {
+    filename: '[name]/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // Add HTML loader to serve HTML files.
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
         ],
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-    },
-    devServer: {
-        contentBase: './dist',
-        writeToDisk: true,
-        compress: true,
-        port: 9000,
-        overlay: true,
-    },
-    devtool: 'cheap-module-source-map',
-    // Call the Plugin here.
-    plugins: [
-        new ErrorOverlayPlugin(),
-    ].concat(multipleHtmlPlugins),
+      },
+    ],
+  },
+  resolve: {
+    modules: ['node_modules', 'app'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  devServer: {
+    contentBase: './dist',
+    writeToDisk: true,
+    compress: true,
+    port: 9000,
+    overlay: true,
+  },
+  devtool: 'cheap-module-source-map',
+  // Call the Plugin here.
+  plugins: [
+    new ErrorOverlayPlugin(),
+  ].concat(multipleHtmlPlugins),
 };
